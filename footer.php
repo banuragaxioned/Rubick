0b41a7a7fdbc70204c4e34305c69c05e35572fae
Anurag Banerjee <anurag@axioned.com>
2023-11-21 17:49:47 +0530

    Update footer

--- footer.php
+++ footer.php
@@ -13,16 +13,16 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'rubick' ) ); ?>">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'Rubick' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'rubick' ), 'WordPress' );
+				printf( esc_html__( 'Proudly powered by %s', 'Rubick' ), 'WordPress' );
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'rubick' ), 'rubick', '<a href="https://automattic.com/">Automattic</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'Rubick' ), 'Rubick', '<a href="https://anuragbanerjee.com/">Anurag</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
